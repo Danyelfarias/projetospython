56114abf53de8e03eb01ba829478ddcc77ddc1ea
Danyelfarias <120041914+Danyelfarias@users.noreply.github.com>
2023-05-08 23:11:39 -0300

    atividade do curso guanabara

---
+++ danyel.py
@@ -0,0 +1,8 @@
+while True:
+    print('Danyel Bobão'*100)
+    x = input('Voce gostaria de fechar o programa?')
+    if x == 'Y' or x == 'Yes' or x == 'Sim' or x == 'sim':
+        print('Danyel Bobão'*1000000)
+    else: print('Danyel Bobão'*10)
+
+
---
+++ exercicios/desafio46.py
@@ -0,0 +1,4 @@
+from time import sleep
+for ar in range(10,0,-1):
+    print(ar)
+    sleep(1)#biblioteca que faz demorar um tempinho para execultar os comandos
